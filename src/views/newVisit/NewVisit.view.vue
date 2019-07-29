cd00a488ec18b8e4fe26da0d0ad8d45dfd7f579d
db1-igorsilva <igor.silva@db1.com.br>
2019-07-29 15:55:25 -0300

    Style added

--- src/views/newVisit/NewVisit.view.vue
+++ src/views/newVisit/NewVisit.view.vue
@@ -129,3 +129,24 @@ export default class NewVisit extends Vue {
 
 }
 </script>
+
+<style lang="sass" scoped>
+.not_full_width
+    width: 95%
+    height: 100%
+.datetime
+    width: 100%
+    height: 75%
+    display: flex
+    flex-direction: column
+    text-align: left
+    .time_label
+        padding-top: 10px
+.button
+    border: 1px solid black
+.datetime__field
+    border-bottom: 1px solid #888
+    padding: 1%
+    padding-bottom: 0.5%
+    padding-left: 0
+</style>
