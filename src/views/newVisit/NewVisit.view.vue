5a52065a602449bdfe60df603f94e7108253ef5c
db1-igorsilva <igor.silva@db1.com.br>
2019-07-29 15:11:54 -0300

    Script added

--- src/views/newVisit/NewVisit.view.vue
+++ src/views/newVisit/NewVisit.view.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="newVisit">
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class NewVisit extends Vue {}
+</script>
