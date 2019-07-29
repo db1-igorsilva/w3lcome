249530f75cb28c51e7517af6f5b7ac4a68ac44b9
db1-igorsilva <igor.silva@db1.com.br>
2019-07-29 15:16:00 -0300

    export const http = axios.create({})

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 
+export const http = axios.create({
+  baseURL: 'http://localhost:3000/'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
