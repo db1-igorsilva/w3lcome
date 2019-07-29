f4d7075f501200421ad70eef011afa1fd8c9a102
db1-igorsilva <igor.silva@db1.com.br>
2019-07-29 15:25:24 -0300

    Menu items added

--- src/shared/Menu.vue
+++ src/shared/Menu.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="menu">
-    
-  </div>
+  <v-toolbar color="#8F1C2C" class="menu__header">
+    <router-link to="/" class="menu__header_db1info">
+        <img class="menu__header" src="https://db1global.sharepoint.com/sites/intranet/Central%20de%20documentos%20PDF/Imagens%20Oficiais/ORG_LOGO_DB1_BRANCO.png"/>
+    </router-link>
+
+    <v-spacer/>
+
+    <v-toolbar-items>
+        <router-link to="/" class="menu__header">
+            <v-btn flat class="menu__header_button" style="color: #FFF"> Visits </v-btn>
+        </router-link>
+        <router-link to="/newVisit" class="menu__header">
+            <v-btn flat class="menu__header_button" style="color: #FFF"> New Visit </v-btn>
+        </router-link>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Menu extends Vue {}
