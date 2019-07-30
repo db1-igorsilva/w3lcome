9c3e3ef57c49adef80e95f91dff2d61efaf6642a
db1-igorsilva <igor.silva@db1.com.br>
2019-07-30 09:37:02 -0300

    Guest Name field received a placeholder, changed to date and time fields pattern and items from table now use get methods

--- src/views/visits/Visits.view.vue
+++ src/views/visits/Visits.view.vue
@@ -1,26 +1,25 @@
 <template>
   <center>
         <div class="not_full_width">
-            <v-flex @input="typedFilter = $event.target.value">
-                <v-text-field label="Guest Name"/>
-            </v-flex>
             <div class="datetime">
-                <label class="datetime time_label"> First Date </label>
-                <input type="date" id="compareStartDate" name="compareStartDate" class="datetime__field" @input="compareStartDate = $event.target.value"/>
-                <label class="datetime time_label"> Last Date </label>
-                <input type="date" id="compareEndDate" name="compareEndDate" class="datetime__field" @input="compareEndDate = $event.target.value"/>
+              <label class="datetime time_label"> Guest Name </label>
+              <input type="text" placeholder="Guest Name" id="compareStartDate" name="compareStartDate" class="datetime__field" @input="typedFilter = $event.target.value"/>
+              <label class="datetime time_label"> First Date </label>
+              <input type="date" id="compareStartDate" name="compareStartDate" class="datetime__field" @input="compareStartDate = $event.target.value"/>
+              <label class="datetime time_label"> Last Date </label>
+              <input type="date" id="compareEndDate" name="compareEndDate" class="datetime__field" @input="compareEndDate = $event.target.value"/>
             </div>
 
             <br>
 
-            <v-data-table :headers="headers" :items="filteredVisits" class="not_full_width elevation-1" name="visits_table">
+            <v-data-table :headers="headers" :items="filteredVisits()" class="not_full_width elevation-1">
                 <template v-slot:items="filteredVisits">
-                    <td> {{ filteredVisits.item.id }} </td>
-                    <td> {{ filteredVisits.item.date }} </td>
-                    <td> {{ filteredVisits.item.presentationStartTime }} </td>
-                    <td> {{ filteredVisits.item.presentationEndTime }} </td>
-                    <td> {{ filteredVisits.item.guest }} </td>
-                    <td> {{ filteredVisits.item.welcomeText }} </td>
+                    <td> {{ filteredVisits.item.getId }} </td>
+                    <td> {{ filteredVisits.item.getDate }} </td>
+                    <td> {{ filteredVisits.item.getPresentationStartTime }} </td>
+                    <td> {{ filteredVisits.item.getPresentationEndTime }} </td>
+                    <td> {{ filteredVisits.item.getGuest }} </td>
+                    <td> {{ filteredVisits.item.getWelcomeText }} </td>
                     <td> <router-link style="text-decoration: none" :to="{name: 'update', params: { id: filteredVisits.item.id }}"> <v-btn class="button"> Update </v-btn> </router-link> </td>
                     <td> <v-btn class="button" @click="remove(filteredVisits.item)"> Delete </v-btn> </td>
                 </template>
