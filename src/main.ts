d7ba49a8e3b43c4dead7399085e6722a871ba231
db1-igorsilva <igor.silva@db1.com.br>
2019-07-30 09:04:35 -0300

    Vuetify now imports 'vuetify' and a import to 'vuetify/dist/vuetify.min.css' was added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import App from '@/App.vue'
 import router from '@/router'
 import axios from 'axios'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  icon: 'md'
+})
 
 export const http = axios.create({
   baseURL: 'http://localhost:3000/'
