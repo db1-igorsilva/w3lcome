8a18c49433f958e9b64fd84f84eb1cd9f709a221
Igor <igorhenss@gmail.com>
2019-07-30 21:40:29 -0300

    All methods created

---
+++ src/domain/guest/Guest.service.ts
@@ -0,0 +1,24 @@
+import { http } from '@/main'
+import Guest from './Guest.entity';
+
+export default class GuestService {
+  static save (guest: Guest) {
+    if (!guest.name) {
+        alert('Every Guest needs a name.')
+        throw new Error('Every Guest needs a name.')
+    }
+    if (!guest.relationshipType) {
+        alert('Every Guest has a Relationship Type.')
+        throw new Error('Every Guest has a Relationship Type.')
+    }
+    return http.post('visitPerson/post', guest)
+  }
+
+  static getAll () {
+    return http.get('guest/getAll')
+  }
+
+  static delete (guest: Guest) {
+    return http.delete('guest/' + guest.id)
+  }
+}
