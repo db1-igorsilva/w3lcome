455095d1280df7c4a4e9b8e3eb581322664f2a8d
Igor <igorhenss@gmail.com>
2019-07-30 21:12:53 -0300

    save method fixed (arguments order adjusted)

--- src/domain/visitPerson/VisitPerson.service.ts
+++ src/domain/visitPerson/VisitPerson.service.ts
@@ -2,9 +2,9 @@ import { http } from '@/main'
 import VisitPerson from '@/domain/visitPerson/VisitPerson.entity'
 
 export default class VisitPersonService {
-  static save (person: string, visit: any) {
+  static save (visit: any, person: string) {
     if (person) {
-      return http.post('visitPerson/post', new VisitPerson(person, visit))
+      return http.post('visitPerson/post', new VisitPerson(visit, person))
     }
   }
 
@@ -12,7 +12,7 @@ export default class VisitPersonService {
     return http.get('visitPerson/getAll')
   }
 
-  static delete (visitPerson: any) {
+  static delete (visitPerson: VisitPerson) {
     return http.delete('visitPerson/' + visitPerson.id)
   }
 }
